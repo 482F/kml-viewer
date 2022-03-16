83737da5811e7574ca52b644fdd372774225d2dd
482F <482F.git.mail@482f.net>
2022-03-17 00:00:00 +0900

    一列目を sticky に

--- src/components/kml-list-header.vue
+++ src/components/kml-list-header.vue
@@ -1,8 +1,9 @@
 <template>
   <v-list-item class="kml-list-header">
-    <template v-for="column of columns">
+    <template v-for="(column, i) of columns">
       <resizer
         :key="column.name"
+        :class="{ 'is-name': i === 0 }"
         :directions="['right']"
         @change="(value) => (column.width = value)"
       >
@@ -40,5 +41,12 @@ export default {
 <style lang="scss" scoped>
 .kml-list-header {
   user-select: none;
+}
+</style>
+
+<style lang="scss" scoped>
+.is-name {
+  position: sticky;
+  left: 0;
 }
 </style>
