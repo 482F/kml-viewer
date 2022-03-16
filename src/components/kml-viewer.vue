a9484eb5cca3af6256bb4c6e3a32f507adb3ac9c
482F <482F.git.mail@482f.net>
2022-03-17 00:00:00 +0900

    kml ファイルを添付して読み込めるように

---
+++ src/components/kml-viewer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    {{ rawKml }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'kml-viewer',
+  props: {
+    rawKml: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
