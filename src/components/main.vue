dc5ef0f41e33daf70739d0a382585143e8ed43cd
482F <0ryonryoko0@gmail.com>
2022-03-19 13:19:25 +0900

    kml ファイルを添付して読み込めるように

--- src/components/main.vue
+++ src/components/main.vue
@@ -1,10 +1,36 @@
 <template>
-  <div>abc</div>
+  <div class="f-main">
+    <v-file-input @change="fileInput" label="kml を添付" outlined dense />
+    <kml-viewer :raw-kml="rawKml" />
+  </div>
 </template>
 
 <script>
+import KmlViewer from './kml-viewer.vue'
+
 export default {
   name: 'main',
-  data: () => ({}),
+  components: {
+    KmlViewer,
+  },
+  data: () => ({
+    rawKml: '',
+  }),
+  methods: {
+    async fileInput(file) {
+      if (!file) {
+        this.rawKml = ''
+      }
+      this.rawKml = await file.text()
+    },
+  },
 }
 </script>
+
+<style lang="scss" scoped>
+.f-main {
+  padding: 4rem;
+  display: flex;
+  flex-direction: column;
+}
+</style>
