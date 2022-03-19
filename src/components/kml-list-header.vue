4a34d701aa7546d83a3405432d7ffae5ad68a261
482F <0ryonryoko0@gmail.com>
2022-03-19 18:42:00 +0900

    ソートができるように

--- src/components/kml-list-header.vue
+++ src/components/kml-list-header.vue
@@ -7,27 +7,48 @@
         :directions="['right']"
         @change="(value) => (column.width = value)"
       >
-        <kml-list-cell :width="column.width" :content="column.name" />
+        <div
+          class="cell"
+          @click="updateSortMethod(column)"
+          :content="column.name"
+          :style="{ '--width': column.width + 'px' }"
+        >
+          <div class="name">
+            {{ column.name }}
+          </div>
+          <v-icon class="icon" v-if="sortMethod.key === column.name">
+            {{ sortMethod.order === 1 ? 'mdi-chevron-up' : 'mdi-chevron-down' }}
+          </v-icon>
+        </div>
       </resizer>
     </template>
   </v-list-item>
 </template>
 
 <script>
-import KmlListCell from './kml-list-cell.vue'
 import Resizer from './resizer.vue'
 
 export default {
   name: 'kml-list-header',
   data() {},
   components: {
-    KmlListCell,
     Resizer,
   },
   props: {
     columns: {
       type: Array,
       required: true,
+    },
+    sortMethod: {
+      type: Object,
+      required: true,
+    },
+  },
+  methods: {
+    updateSortMethod(column) {
+      const key = column.name
+      const order = this.sortMethod.key === key ? this.sortMethod.order * -1 : 1
+      this.$emit('update:sort-method', { key, order })
     },
   },
 }
@@ -36,6 +57,24 @@ export default {
 <style lang="scss" scoped>
 .kml-list-header {
   user-select: none;
+  .cell {
+    cursor: pointer;
+    padding: 0 8px;
+    height: 48px;
+    transition: background-color 0.2s;
+    width: var(--width);
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    &:hover {
+      background-color: lightgray;
+    }
+    .name {
+      text-overflow: ellipsis;
+      white-space: nowrap;
+      overflow: hidden;
+    }
+  }
 }
 </style>
 
