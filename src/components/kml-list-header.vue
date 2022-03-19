64359495a59d8c342f406b71b230b7ecfe10387c
482F <0ryonryoko0@gmail.com>
2022-03-19 19:31:10 +0900

    列を非表示にできるように

--- src/components/kml-list-header.vue
+++ src/components/kml-list-header.vue
@@ -1,27 +1,61 @@
 <template>
   <v-list-item class="kml-list-header">
-    <template v-for="(column, i) of columns">
-      <resizer
-        :key="column.name"
-        :class="{ 'is-name': i === 0 }"
-        :directions="['right']"
-        @change="(value) => (column.width = value)"
-      >
-        <div
-          class="cell"
-          @click="updateSortMethod(column)"
-          :content="column.name"
-          :style="{ '--width': column.width + 'px' }"
+    <div class="left">
+      <template v-for="(column, i) of columns">
+        <resizer
+          v-show="column.show"
+          :key="column.name"
+          :class="{ 'is-name': i === 0 }"
+          :directions="['right']"
+          @change="(value) => (column.width = value)"
         >
-          <div class="name">
-            {{ column.name }}
+          <div
+            class="cell"
+            @click="updateSortMethod(column)"
+            :content="column.name"
+            :style="{ '--width': column.width + 'px' }"
+          >
+            <div class="name">
+              {{ column.name }}
+            </div>
+            <v-icon class="icon" v-if="sortMethod.key === column.name">
+              {{
+                sortMethod.order === 1 ? 'mdi-chevron-up' : 'mdi-chevron-down'
+              }}
+            </v-icon>
+            <v-icon
+              class="icon show-on-hover"
+              v-else
+              @click="column.show = false"
+            >
+              mdi-close
+            </v-icon>
           </div>
-          <v-icon class="icon" v-if="sortMethod.key === column.name">
-            {{ sortMethod.order === 1 ? 'mdi-chevron-up' : 'mdi-chevron-down' }}
-          </v-icon>
+        </resizer>
+      </template>
+    </div>
+    <div class="right">
+      <v-menu offset-y>
+        <template v-slot:activator="{ on }">
+          <v-btn class="icon-button" icon v-on="on">
+            <v-icon>mdi-menu</v-icon>
+          </v-btn>
+        </template>
+        <div class="kml-list-header-menu">
+          <v-list>
+            <v-list-item
+              v-for="column of columns"
+              :key="column"
+              v-show="!column.show"
+              @click="column.show = true"
+              :ripple="false"
+            >
+              {{ column.name }}
+            </v-list-item>
+          </v-list>
         </div>
-      </resizer>
-    </template>
+      </v-menu>
+    </div>
   </v-list-item>
 </template>
 
@@ -57,8 +91,27 @@ export default {
 <style lang="scss" scoped>
 .kml-list-header {
   user-select: none;
+  display: flex;
+  justify-content: space-between;
+  .right,
+  .left {
+    display: flex;
+    width: fit-content;
+  }
+  .right {
+    flex-grow: 1;
+    height: 48px;
+    justify-content: flex-end;
+    align-items: center;
+    position: sticky;
+    right: 0;
+    > * {
+      background-color: white;
+    }
+  }
   .cell {
     cursor: pointer;
+    background-color: white;
     padding: 0 8px;
     height: 48px;
     transition: background-color 0.2s;
@@ -75,12 +128,31 @@ export default {
       overflow: hidden;
     }
   }
+  .icon-button {
+    position: sticky;
+    right: 0;
+  }
 }
 </style>
 
 <style lang="scss" scoped>
-.is-name {
-  position: sticky;
-  left: 0;
+.kml-list-header {
+  .is-name {
+    position: sticky;
+    left: 0;
+  }
+  .cell {
+    &:not(:hover) {
+      .show-on-hover {
+        display: none;
+      }
+    }
+  }
+}
+</style>
+
+<style lang="scss">
+.kml-list-header-menu {
+  background-color: white;
 }
 </style>
