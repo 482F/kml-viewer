e3ca4bf56ea37000855aac414407653d6587e252
482F <482F.git.mail@482f.net>
2022-04-08 00:00:00 +0900

    @atoms alias を追加

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,18 @@
+const path = require('path')
+
 module.exports = {
   publicPath:
     process.env.NODE_ENV === 'production'
       ? '/kml-viewer/' //任意
       : '/',
   outputDir: 'dist/latest',
-  transpileDependencies: [
-    'vuetify'
-  ]
+  transpileDependencies: ['vuetify'],
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, 'src'),
+        '@atoms': path.join(__dirname, 'src/components/atoms'),
+      },
+    },
+  },
 }
