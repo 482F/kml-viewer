65c727176af1982e3cecd94139405aab565af2c9
482F <482F.git.mail@482f.net>
2022-04-08 00:00:00 +0900

    atom を追加

---
+++ src/components/atoms/a-icon.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-icon>
+    {{ icon }}
+  </v-icon>
+</template>
+
+<script>
+export default {
+  name: 'a-icon',
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+  },
+}
+</script>
+
