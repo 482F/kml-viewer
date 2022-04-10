124e5ce874773267038982bd1d3224b1cf369e33
482F <0ryonryoko0@gmail.com>
2022-04-10 17:35:09 +0900

    atom を追加

---
+++ src/components/atoms/a-button.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-btn
+    :outlined="outlined"
+    :icon="icon"
+    small
+    @click="$emit('click')"
+    @mouseenter="$emit('mouseenter')"
+    @mouseleave="$emit('mouseleave')"
+  >
+    {{ label }}
+    <slot />
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'a-button',
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+    icon: {
+      type: Boolean,
+      default: false,
+    },
+    outlined: {
+      type: Boolean,
+      default: true,
+    },
+  },
+}
+</script>
---
+++ src/components/atoms/a-icon-button.vue
@@ -0,0 +1,33 @@
+<template>
+  <v-tooltip bottom :disabled="!tooltip">
+    <template v-slot:activator="{ on, attrs }">
+      <a-button v-on="on" v-bind="attrs" icon :outlined="false">
+        <a-icon :icon="icon" />
+      </a-button>
+    </template>
+    {{ tooltip }}
+  </v-tooltip>
+</template>
+
+<script>
+import AButton from '@atoms/a-button.vue'
+import AIcon from '@atoms/a-icon.vue'
+
+export default {
+  name: 'a-icon-button',
+  components: {
+    AButton,
+    AIcon,
+  },
+  props: {
+    icon: {
+      type: String,
+      required: true,
+    },
+    tooltip: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
