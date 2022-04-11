2036abba89b1bbc523350974bfbfae7c068dbfaa
482F <0ryonryoko0@gmail.com>
2022-04-11 21:27:10 +0900

    最新版が動作していなかった問題を修正

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
   publicPath:
     process.env.NODE_ENV === 'production'
-      ? '/kml-viewer/' //任意
+      ? './' //任意
       : '/',
   outputDir: 'dist/latest',
   transpileDependencies: ['vuetify'],
