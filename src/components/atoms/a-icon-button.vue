0f10956a48d8c2e6f63db25cb13910e1b904bf7c
482F <482F.git.mail@482f.net>
2022-08-26 07:30:56 +0900

    バージョンのアイコンを修正

--- src/components/atoms/a-icon-button.vue
+++ src/components/atoms/a-icon-button.vue
@@ -1,12 +1,14 @@
 <template>
-  <v-tooltip bottom :disabled="!tooltip">
-    <template v-slot:activator="{ on, attrs }">
-      <a-button v-on="on" v-bind="attrs" icon :outlined="false">
-        <a-icon :icon="icon" />
-      </a-button>
-    </template>
-    {{ tooltip }}
-  </v-tooltip>
+  <span>
+    <v-tooltip bottom :disabled="!tooltip">
+      <template v-slot:activator="{ on, attrs }">
+        <a-button v-on="on" v-bind="attrs" icon :outlined="false">
+          <a-icon :icon="icon" />
+        </a-button>
+      </template>
+      {{ tooltip }}
+    </v-tooltip>
+  </span>
 </template>
 
 <script>
